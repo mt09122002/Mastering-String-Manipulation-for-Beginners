9d2b93813c0938a9dfe17b37f071aa7f9e5bba67
LeMinhTuong <102809207+MinhTuongLe@users.noreply.github.com>
2024-10-21 21:54:34 +0700

    update

---
+++ CounttheNumberofWordsinaSentence.py
@@ -0,0 +1,13 @@
+# Write a Python program that counts the number of words in that sentence.
+
+# Step 1: The sentence
+sentence = "Python ID - Online Python Compiler"
+
+# Step 2: Split the sentence into words
+words = sentence.split()
+
+# Step 3: Count the number of words
+word_count = len(words)
+
+# Step 4: Display the result
+print(f"The number of words in the sentence is: {word_count}")
---
+++ ReplaceaSubstringinaString.py
@@ -0,0 +1,14 @@
+# Write a Python program that replace all occurrences of the first word with the second word and print the result.
+
+# Step 1: The sentence
+sentence = "Python ID - Online Python Compiler"
+
+# Step 2: The word to replace and the word to replace it with
+word_to_replace = "Online"
+replacement_word = "Helpful"
+
+# Step 3: Replace all occurrences of the word
+updated_sentence = sentence.replace(word_to_replace, replacement_word)
+
+# Step 4: Display the updated sentence
+print(f"Updated sentence: {updated_sentence}")
